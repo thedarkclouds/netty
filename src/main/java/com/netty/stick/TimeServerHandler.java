e91f965bb90b7999329e7c844a419ff4092d1d5d
huyue <17366009279@139.com>
2019-01-06 14:32:08 +0800

    netty 粘包问题 1.0

---
+++ src/main/java/com/netty/stick/TimeServerHandler.java
@@ -0,0 +1,40 @@
+package com.netty.stick;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.util.Date;
+/*
+* 粘包  现象
+*
+* 按照设计：服务端收到的消息应该和客户端的消息总数相同
+* */
+public class TimeServerHandler extends ChannelHandlerAdapter {
+    private int counter;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        String body = new String(req, "UTF-8").
+                substring(0, req.length - System.getProperty("line.separator").length());
+        System.out.println("This time server receive order:" + body + "; the counter is " + ++counter);
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+        currentTime = currentTime + System.getProperty("line.separator");
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.write(resp);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
